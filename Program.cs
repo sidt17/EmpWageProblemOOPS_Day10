ce6d69568e26f8f9411d1d31c00426d09f8390d6
Siddhant <santoshshrivastava39@gmail.com>
2022-04-07 00:34:47 +0530

    Call in main method

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             EmpWage obj1 = new EmpWage();
-            obj1.Wage();
+            obj1.computeWage("Cognizant", 20, 20, 100);
+            obj1.computeWage("HCL", 22, 25, 100);
+            obj1.computeWage("Cognizant", 20, 20, 100);
         }
     }
 }
