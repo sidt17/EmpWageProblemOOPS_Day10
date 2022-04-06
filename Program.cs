34f402de48502785507bb042e7161ca3515dd305
Siddhant <santoshshrivastava39@gmail.com>
2022-04-07 00:20:02 +0530

    Call in main method

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWage obj1 = new EmpWage();
+            obj1.Wage();
+        }
+    }
+}
